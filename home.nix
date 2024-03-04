09f0f92df32187c96db0fd71031593a6258034ca
iamcult <101368650+iamcult@users.noreply.github.com>
2024-03-04 16:53:46 -0500

    feat: add starship

--- home.nix
+++ home.nix
@@ -60,12 +60,14 @@
     };
   };
 
+  programs.starship.enable = true;
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       alias nixos-version="nix profile history --profile /nix/var/nix/profiles/system | tail -2 | grep Version | rev | cut -c 20- | rev"
-      alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos-config#thing && pushd ~/nixos-config; git commit -a; popd" 
+      alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos-config#thing && pushd ~/nixos-config; git commit -a; popd"
+      starship init fish | source 
     '';
   };
 
