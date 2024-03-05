0456c8a6c201213ca7874e343fcb00000da072f6
iamcult <101368650+iamcult@users.noreply.github.com>
2024-03-05 09:41:35 -0500

    feat: add push-os alias

--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@
       set fish_greeting # Disable greeting
       alias nixos-version="nix profile history --profile /nix/var/nix/profiles/system | tail -2 | grep Version | rev | cut -c 20- | rev"
       alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos-config#thing && pushd ~/nixos-config; git commit -a; popd"
+      alias push-os="pushd ~/nixos-config; git push; popd"
       alias clear="clear && pfetch"
       pfetch
       starship init fish | source 
