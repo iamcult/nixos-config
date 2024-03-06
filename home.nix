3b0316cc344ca1d7ad38903e1945082362349e3c
iamcult <101368650+iamcult@users.noreply.github.com>
2024-03-05 21:34:02 -0500

    fix: add commit-os alias

--- home.nix
+++ home.nix
@@ -22,7 +22,8 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       alias nixos-version="nix profile history --profile /nix/var/nix/profiles/system | tail -2 | grep Version | rev | cut -c 20- | rev"
-      alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos-config#thing && pushd ~/nixos-config; git commit -a; popd"
+      alias commit-os="pushd ~/nixos-config; git commit -a; popd"
+      alias rebuild-os="doas nixos-rebuild switch --flake ~/nixos-config#thing && commit-os"
       alias push-os="pushd ~/nixos-config; git push; popd"
       alias clear="clear && pfetch"
       pfetch
